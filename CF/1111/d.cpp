9a68c3d4b6104e60be2e14bf2d37302e848d9d56
cwl769 <cwl070609@outlook.com>
2026-08-15 18:35:24 +0800

    cf 1111d

--- CF/1111/d.cpp
+++ CF/1111/d.cpp
@@ -1,4 +1,3 @@
-#include <cassert>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -36,65 +35,6 @@ void readInt(T& x, Args&... args) {
 }
 
 const int inf = 0x3f3f3f3f;
-const i64 inf64 = 0x3f3f3f3f3f3f3f3f;
-
-struct Segtree {
-    struct Node {
-        int mn, mx;
-        Node *son[2];
-        Node() : mn(inf), mx(0), son{NULL, NULL} {}
-        void pushup() {
-            mn = inf;
-            mx = 0;
-            if (son[0]) {
-                mn = std::min(mn, son[0]->mn);
-                mx = std::max(mx, son[0]->mx);
-            }
-            if (son[1]) {
-                mn = std::min(mn, son[1]->mn);
-                mx = std::max(mx, son[1]->mx);
-            }
-        }
-        void change(int l, int r, int x, int v) {
-            if (l == r) {
-                this->mx = this->mn = v;
-                return;
-            }
-            int mid = ((l + r) >> 1);
-            if (x <= mid)
-                son[0]->change(l, mid, x, v);
-            else
-                son[1]->change(mid + 1, r, x, v);
-            this->pushup();
-        }
-        void maketree(int l, int r, int *arr) {
-            if (l == r) {
-                mn = mx = arr[l];
-                return;
-            }
-            int mid = ((l + r) >> 1);
-            son[0] = new Node;
-            son[1] = new Node;
-            son[0]->maketree(l, mid, arr);
-            son[1]->maketree(mid + 1, r, arr);
-            this->pushup();
-        }
-    };
-
-    int L, R;
-    Node *root;
-    Segtree(int l, int r) {
-        L = l, R = r;
-        root = new Node;
-    }
-    void maketree(int *arr) { root->maketree(L, R, arr); }
-    void change(int x, int v) {
-        assert(L <= x && x <= R);
-        root->change(L, R, x, v);
-    }
-    int min() { return root->mn; }
-    int max() { return root->mx; }
-};
 
 void expand(veci &a) {
     int n = a.size();
@@ -108,12 +48,10 @@ void expand(veci &a) {
     a.resize(tar, inf);
 }
 
-int log2(int x) {
+int mylog2(int x) {
     int ans = 0;
-    while (x > 1) {
+    for (;x > 1; x >>= 1)
         ++ans;
-        x >>= 1;
-    }
     return ans;
 }
 
@@ -124,68 +62,107 @@ void solve() {
     for (int i = 0; i < n; ++i)
         readInt(a[i]);
     expand(a);
+
     n = a.size();
-    int lgn = log2(n);
-    // printf("log2(%d) = %d\n", n, lgn);
-    std::vector<std::vector<Segtree>> tree(lgn);
-    veci cnt(lgn + 1);
-    for (int i = 0; i < lgn; ++i) {
-        int len = (1 << i);
-        for (int st = 0; st + len <= n; st += len) {
-            tree[i].emplace_back(st, st + len - 1);
-            tree[i].back().maketree(&a[0]);
+    int lgn = mylog2(n);
+    // printf("%d %d\n", n, lgn);
+
+    if(n == 1) {
+        // printf("q = %d\n", q);
+        printf("0\n");
+        while(q--) {
+            int p, x;readInt(p, x);
+            printf("0\n");
         }
-        for (int j = 1; j < n / len; ++j) {
-            if (tree[i][j - 1].max() > tree[i][j].min())
-                ++cnt[i];
+        return;
+    }
+
+    std::vector<veci> mn(lgn);
+    std::vector<veci> mx(lgn);
+    veci cnt(lgn);
+    mn[0].resize(n);
+    mx[0].resize(n);
+    for (int i = 0; i < n; ++i)
+        mn[0][i] = mx[0][i] = a[i];
+    for (int i = 1; i < n; ++i)
+        if (mx[0][i - 1] > mn[0][i])
+            ++cnt[0];
+    for (int t = 1; t < lgn; ++t) {
+        int len = (1 << t);
+        int tot = n / len;
+        mn[t].resize(tot);
+        mx[t].resize(tot);
+        // printf("t = %d| %d %d\n", t, len, tot);
+        for (int i = 0; i < tot; ++i) {
+            mn[t][i] = std::min(mn[t-1][(i << 1)], mn[t-1][(i << 1) ^ 1]);
+            mx[t][i] = std::max(mx[t-1][(i << 1)], mx[t-1][(i << 1) ^ 1]);
+        }
+        for (int i = 1; i < tot; ++i) {
+            if (mx[t][i - 1] > mn[t][i])
+                ++cnt[t];
         }
     }
 
-    auto update = [n, lgn, &tree, &a, &cnt](int p, int x) -> void {
-        for (int t = 0; t < lgn; ++t) {
-            int len = (1 << t);
-            int bl = p / len;
-            if (bl != 0) {
-                if (tree[t][bl - 1].max() > tree[t][bl].min())
-                    --cnt[t];
-            }
-            if (bl != n / len - 1) {
-                if (tree[t][bl].max() > tree[t][bl + 1].min())
-                    --cnt[t];
-            }
-
-            tree[t][bl].change(p, x);
-            
-            if (bl != 0) {
-                if (tree[t][bl - 1].max() > tree[t][bl].min())
-                    ++cnt[t];
-            }
-            if (bl != n / len - 1) {
-                if (tree[t][bl].max() > tree[t][bl + 1].min())
-                    ++cnt[t];
-            }
-        }
-        a[p] = x;
-    };
-
-
-    for (int i = 0; i <= lgn; ++i) {
-        if (cnt[i] == 0) {
-            printf("%d\n", (1 << i) >> 1);
-            break;
-        }
-    }
+    int anst = 0;
+    while (anst < lgn && cnt[anst])
+        ++anst;
+    printf("%d\n", (1 << anst) >> 1);
 
     while (q--) {
         int p, x;
         readInt(p, x);
-        update(p, x);
-        for (int i = 0; i <= lgn; ++i) {
-            if (cnt[i] == 0) {
-                printf("%d\n", (1 << i) >> 1);
-                break;
+
+        if (p > 0) {
+            if (mx[0][p - 1] > mn[0][p])
+                --cnt[0];
+        }
+        if (p + 1 < n) {
+            if (mx[0][p] > mn[0][p + 1])
+                --cnt[0];
+        }
+        mn[0][p] = mx[0][p] = x;
+        if (p > 0) {
+            if (mx[0][p - 1] > mn[0][p])
+                ++cnt[0];
+        }
+        if (p + 1 < n) {
+            if (mx[0][p] > mn[0][p + 1])
+                ++cnt[0];
+        }
+
+        for (int t = 1; t < lgn; ++t) {
+            int len = (1 << t);
+            int tot = n / len;
+            int bl = p / len;
+            if (bl > 0) {
+                if (mx[t][bl - 1] > mn[t][bl])
+                    --cnt[t];
+            }
+            if (bl + 1 < tot) {
+                if (mx[t][bl] > mn[t][bl + 1])
+                    --cnt[t];
+            }
+
+            mn[t][bl] = std::min(mn[t-1][(bl << 1)], mn[t-1][(bl << 1) ^ 1]);
+            mx[t][bl] = std::max(mx[t-1][(bl << 1)], mx[t-1][(bl << 1) ^ 1]);
+            
+            if (bl > 0) {
+                if (mx[t][bl - 1] > mn[t][bl])
+                    ++cnt[t];
+            }
+            if (bl + 1 < tot) {
+                if (mx[t][bl] > mn[t][bl + 1])
+                    ++cnt[t];
             }
         }
+        
+
+        a[p] = x;
+
+        anst = 0;
+        while (anst < lgn && cnt[anst])
+            ++anst;
+        printf("%d\n", (1 << anst) >> 1);
     }
 }
 
